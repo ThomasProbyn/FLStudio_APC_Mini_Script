4b98e4dac9ffe1d63f3503fa0288a820bc433f74
ThomasProbyn <49936803+ThomasProbyn@users.noreply.github.com>
2020-06-06 14:53:07 +0100

    Added some new features, General bug fixes

--- device_apcmini.py
+++ device_apcmini.py
@@ -1,21 +1,26 @@
-#name=AKAI APC Mini (Mixer Control)
+#name=AKAI APC Mini (Mixer Control/FPC)
 
 # Import the required modules
 
-import mixer 
+import mixer
 import midi
 import device
 import ui
 
 def OnInit():
-	global selectedBank
-	selectedBank = 1
+	global selectedBank #Make the selectedBank Variable
+	selectedBank = 1 #Default the bank selector to 1
+	clearAllLEDs() #Clear LEDs that may have been left on by other programs force-closing
+	device.midiOutMsg(0x90 + (0x40 << 8) + (0x01 << 16)) #Turn on the LED for bank 1
+	lightFPC() #Turn on the 4*4 matrix for the FPC controller in the top corner.
+	#startTransport() #Start polling the transport
+
+def OnDeInit(): #When FL is closed, clean up everything
 	clearAllLEDs()
-	device.midiOutMsg(0x90 + (0x40 << 8) + (0x01 << 16))
 
 
 def OnNoteOn(event):   #Let's tell FL what to do when it recieves a note on event through midi
-	#print('Midi note on:', event.data1, " ", event.data2)
+	print('Midi note on:', event.data1, " ", event.data2)
 	if event.data1 > 63 and event.data1 < 81 or event.data1 > 81 and event.data1 < 88:	#Check to see if the note is in the range used by the patch selector
 		event.handled = True #Start by telling FL we are dealing with this note to stop it from playing a tone
 		setPatchBank(event.data1) #If it is, pass it through to the function that selects patches
@@ -31,6 +36,7 @@ def OnNoteOff(event):	#Tell FL what to do with note off data
 	event.handled = True	#Not much here, just stop FL from getting too excited and playing a tone
 
 
+
 def OnControlChange(event):	 #Let's define what FL will do when a slider moves
 	if (event.pmeFlags & midi.PME_System != 0):	#Not entirely sure what this does, (pretty certian it rate limits) but it seems to improve performance, so I'll leave it in.
 		mixer.setTrackVolume(bankSliderToChan(selectedBank, event.data1), event.data2/127) #Set the mixer track volume according to the input
@@ -40,10 +46,12 @@ def setPatchBank(bank): 	#This allows us to set the patch bank we are using
 	if bank < 80:	#See if we are in the bottom row
 		selectedBank = bank - 63	#perform the expression needed to convert from midi to mixer channel data
 		clearAllLEDs() #Clears the LEDs that are on
+		lightFPC() #Reilluminates the FPC
 		device.midiOutMsg(0x90 + (bank << 8) + (0x01 << 16)) #Turns on the appropriate LED
 	if bank > 80:	#See if we are in the side row
 		selectedBank = bank - 73	#perform the expression needed to convert from midi to mixer channel data
 		clearAllLEDs() #Clears all of the LEDs
+		lightFPC() #Reilluminates the FPC
 		device.midiOutMsg(0x90 + (bank << 8) + (0x01 << 16))
 	return selectedBank	#Return the selected bank as an integer.
 
@@ -54,3 +62,15 @@ def clearAllLEDs():
 	LEDNumber = 0
 	for LEDNumber in range(0, 89):
 		device.midiOutMsg(0x90 + (LEDNumber << 8) + (0x00 << 16))
+
+def lightFPC():
+	fpcpadsbase = [60, 52, 44, 36]
+	for x in range(0, len(fpcpadsbase)):
+		for y in range(0, 4):
+			device.midiOutMsg(0x90 + (fpcpadsbase[x]+y << 8) + (0x05 << 16))
+
+def tempoUp():
+	pass
+
+def tempoDown():
+	pass
